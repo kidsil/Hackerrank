6ddf3fa5366f0d87266984595a1a170d35853e9e
Asaf Zamir <asaf.zamir@gmail.com>
2018-10-31 16:22:12 +0100

    Reducing the number of rotation by calculating the mod of rotations % fullRotation fullRotation is the number of steps it takes for a given circle to return to its original state
    reduced time from ~93 seconds (93645ms)
    to ~9.4 seconds (9437ms). Nearly 10X time reduction.
    out of which 2720ms is only input reading (Testcase #7).

--- matrix-rotation/index.js
+++ matrix-rotation/index.js
@@ -32,6 +32,19 @@ function renderMatrix(matrix) {
     return str;
 }
 
+//Updating only the defined values
+function updateMatrix(matrix, newMatrix) {
+    for (let i = 0; i < newMatrix.length; i++) {
+        if (Array.isArray(newMatrix[i])) {
+            for (let j = 0; j < newMatrix[i].length; j++) {
+                if (typeof newMatrix[i][j] !== 'undefined') {
+                    matrix[i][j] = newMatrix[i][j];
+                }
+            }
+        }
+    }
+}
+
 function moveElem(newMatrix, i, j, matrix, dir) {
     //Changing from Matrix locations to Array indexes
     i = i - 1;
@@ -53,6 +66,10 @@ function moveElem(newMatrix, i, j, matrix, dir) {
             if (!Array.isArray(newMatrix[i + 1])) newMatrix[i + 1] = [];
             newMatrix[i + 1][j] = matrix[i][j];
             break;
+        case 'none':
+            if (!Array.isArray(newMatrix[i])) newMatrix[i] = [];
+            newMatrix[i][j] = matrix[i][j];
+            break;
     }
 
 }
@@ -62,26 +79,44 @@ function moveElem(newMatrix, i, j, matrix, dir) {
 function matrixRotation(matrix, m, n, r) {
     //number of circles (smaller rows/columns number divided by 2);
     const circleNum = parseInt(Math.min.apply(null, [m, n]) / 2);
-    //How many rotations until the matrix is back to its original state
-
-    let newMatrix;
-    for (let rotation = 0; rotation < r; rotation++) {
-        newMatrix = [];
-        for (let c = 0; c < circleNum; c++) {
+    let newMatrix, matrixBuffer, fullRotation, multiplier, rotations, dir;
+    newMatrix = [];
+    for (let c = 0; c < circleNum; c++) {
+        matrixBuffer = matrix.slice();
+        //Multiplier for the full rotation equation
+        multiplier = (c * 2) + 1;
+        //Calculating fullRotation - the number of steps it takes for a full rotation of the circle
+        fullRotation = 2 * (n - multiplier) + 2 * (m - multiplier);
+        //Getting the remainder from a division of total rotations by the circle's fullRotation number (as it's meaningless to repeatedly rotate a fullRotation)
+        rotations = r % fullRotation;
+        console.log('fullrotation: ' + fullRotation + ' rotations: ' + rotations);
+        dir = '';
+        //If rotations = 0 we need to just set the elements as they are on the newMatrix, we run 1 rotation with direction set to 'none'
+        if (rotations == 0) {
+            dir = 'none';
+            rotations = 1;
+        }
+        for (let rotation = 0; rotation < rotations; rotation++) {
             for (let i = c + 1; i < m - c; i++) {
-                moveElem(newMatrix, i, c + 1, matrix, 'down');
-                moveElem(newMatrix, i + 1, n - c, matrix, 'up');
+                if (dir !== 'none') dir = 'down';
+                moveElem(newMatrix, i, c + 1, matrixBuffer, dir);
+                if (dir !== 'none') dir = 'up';
+                moveElem(newMatrix, i + 1, n - c, matrixBuffer, dir);
             }
             for (let j = c + 1; j < n - c; j++) {
-                moveElem(newMatrix, c + 1, j + 1, matrix, 'left');
-                moveElem(newMatrix, m - c, j, matrix, 'right');
+                if (dir !== 'none') dir = 'left';
+                moveElem(newMatrix, c + 1, j + 1, matrixBuffer, dir);
+                if (dir !== 'none') dir = 'right';
+                moveElem(newMatrix, m - c, j, matrixBuffer, dir);
             }
+
+            updateMatrix(matrixBuffer, newMatrix);
+
         }
-        matrix = newMatrix.slice();
     }
     return newMatrix;
 }
-
+const initialTime = Date.now();
 function main() {
     const ws = fs.createWriteStream('result.txt');
     const mnr = readLine().replace(/\s+$/g, '').split(' ');
@@ -98,8 +133,9 @@ function main() {
         matrix[i] = readLine().replace(/\s+$/g, '').split(' ').map(matrixTemp => parseInt(matrixTemp, 10));
     }
 
+    console.log('PRE RENDER - Time passed: ' + (Date.now() - initialTime));
     const result = renderMatrix(matrixRotation(matrix, m, n, r));
-
+    console.log('END - Time passed: ' + (Date.now() - initialTime));
     ws.write(result);
     ws.end();
 }
