b588139d1170bf94bd6913ef4d1894971ef8b584
Asaf Zamir <asaf.zamir@gmail.com>
2018-12-12 22:06:35 +0100

    adding ctci-bubble-sort

---
+++ sorting/ctci-bubble-sort/index.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the countSwaps function below.
+function countSwaps(a) {
+    let swaps = 0;
+    for (let i = 0; i < a.length; i++) {
+        for (let j = 0; j < a.length - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                let temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+                swaps++;
+            }
+        }
+        
+    }
+    console.log(swaps);
+    return 'Array is sorted in ' + swaps + ' swaps. \n' +
+    'First Element: ' + a[0] + ' \n ' +
+    'Last Element: ' + a[a.length - 1]
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+    const n = parseInt(readLine(), 10);
+
+    const a = readLine().split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    ws.write(countSwaps(a) + "\n");
+
+    ws.end();
+}
